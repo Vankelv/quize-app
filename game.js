40fa58c3333e80a197d56fe0e62a8d53741c35df
Kelvin Samuel Klutse <nextlinedesigns@gmail.com>
2023-04-21 11:01:28 +0000

    update

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -57,25 +58,41 @@ startGame = () => {
   getNewQuestion();
 };
 getNewQuestion = () => {
-    if(availableQuestions.length== 0 || questionCounter >= MAX_QUESTIONS){
-        return window.location.assign('/end.html');
-    }
-    questionCounter++;
-    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-    // questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
+  // If there are no more questions or the maximum number of questions has been reached
+  if (availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS) {
+      // Save the final score to local storage
+      localStorage.setItem("mostRecentScore", score);
+      // Redirect to the end page
+      return window.location.assign('/end.html');
+  }
 
-    const quenstionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[quenstionIndex];
-    question.innerText = currentQuestion.question;
-  
-    choices.forEach((choice) => {
+  // Increment the question counter and update the progress text and bar
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  // Choose a random question from the remaining available questions
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  // Update the answer choices for the current question
+  choices.forEach((choice) => {
       const number = choice.dataset["number"];
       choice.innerText = currentQuestion["choice" + number];
-    });
+  });
 
-    availableQuestions.splice(quenstionIndex, 1);
-    acceptingAnswers = true;
-  };
+  // Remove the current question from the available questions array
+  availableQuestions.splice(questionIndex, 1);
+
+  // Save the current score and remaining questions to local storage
+  localStorage.setItem("currentScore", score);
+  localStorage.setItem("remainingQuestions", JSON.stringify(availableQuestions));
+
+  // Set acceptingAnswers to true to allow the user to select an answer
+  acceptingAnswers = true;
+};
+
 
   choices.forEach(choice => {
     choice.addEventListener("click", e =>{
